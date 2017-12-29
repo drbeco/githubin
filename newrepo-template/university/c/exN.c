fbb9676f316d6d061db7e4aed959a38801927f6f
Ruben Carlo Benante <rcb@beco.cc>
2017-12-29 11:42:07 -0300

    vim modeline

--- newrepo-template/university/c/exN.c
+++ newrepo-template/university/c/exN.c
@@ -281,7 +281,8 @@ void exN_init(void)
     return;
 }
 
-/* ---------------------------------------------------------------------------- */
-/* vi: set ai cin et ts=4 sw=4 tw=0 wm=0 fo=croqltn : C config for Vim modeline */
-/* Template by Dr. Beco <rcb at beco dot cc>  Version 20160714.153029           */
+/* ------------------------------------------------------------------------- */
+/* C config for Vim modeline                                                 */
+/* vi: set ai cin et ts=8 sts=4 sw=4 tw=79 wm=0 cc=+1 lbr fo=croq :          */
+/* Template by Dr. Beco <rcb at beco dot cc>       Version 20170724.223312   */
 
