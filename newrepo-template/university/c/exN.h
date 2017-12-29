4cc7fa0e4cb2e40490cc73765767c6700e44d70e
Ruben Carlo Benante <rcb@beco.cc>
2017-12-29 13:16:26 -0300

    (C) 2016, 2017, 2018...

--- newrepo-template/university/c/exN.h
+++ newrepo-template/university/c/exN.h
@@ -2,7 +2,7 @@
  *   exN.h                                    Version 20160908.182854      *
  *                                                                         *
  *   Brief description                                                     *
- *   Copyright (C) 2016         by Ruben Carlo Benante                     *
+ *   Copyright (C) 2016-2018    by Ruben Carlo Benante                     *
  ***************************************************************************
  *   This program is free software; you can redistribute it and/or modify  *
  *   it under the terms of the GNU General Public License as published by  *
