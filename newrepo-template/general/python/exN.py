c4aac54634dded4424fedb4ac6708c2f2ae19e72
Ruben Carlo Benante <rcb@beco.cc>
2018-05-06 16:13:57 -0300

    general/python, university/prolog

---
+++ newrepo-template/general/python/exN.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+"""
+Python source code - brief description
+Copyright (C) YEAR - Autor
+Contact
+Date
+"""
+
+def main():
+    print('main function')
+
+if __name__ == "__main__":
+    main()
+
+#/* ------------------------------------------------------------------------ */
+#/* Python config for Vim modeline                                           */
+#/* vi: set ai sta et ts=8 sts=4 sw=4 tw=79 wm=0 cc=+1 lbr fo=croq :         */
+#/* Template by Dr. Beco <rcb at beco dot cc>        Version 20170724.161751 */
+
---
+++ newrepo-template/university/prolog/README.md
@@ -0,0 +1,25 @@
+## Nome do Repositório
+
+### Introdução ao problema
+
+* Colocar aqui uma breve explicação do programa em PROLOG
+* Explicar o processamento
+
+### Uso
+
+* Colocar aqui a sintáxe de uso do programa
+* Objetivos e condições de entrada, entradas esperadas
+* Soluções, saída produzida
+
+### Exemplos
+
+* Colocar aqui alguns exemplos
+* Casos de uso
+
+### Orientação
+
+* Autor: Prof. Dr. Ruben Carlo Benante
+* Email: rcb@upe.br
+* Data: 2017-05-09
+* Licensa: GNU/GPL v2.0
+
